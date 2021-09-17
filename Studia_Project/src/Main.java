c2db0e17b68f1bc581700e991201c28f573012e9
DevXord <xordii.xd@gmail.com>
2021-09-17 20:04:12 +0200

    CW_2 Commit

--- Studia_Project/src/Main.java
+++ Studia_Project/src/Main.java
@@ -1,7 +1,16 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Podaj tekst: ");
+        String name = sc.nextLine();
+        System.out.println("Podaj liczbę: ");
+        int number = sc.nextInt();
+        System.out.println("Tekst: " + name + ", " + "Liczba: " + number);
+        System.out.printf("Tekst: %s, Liczba: %d",name,number);
+
     }
 
 }
