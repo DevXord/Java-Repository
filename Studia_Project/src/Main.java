9a17f4716dc9de83c596af356e42250e815cd2ac
DevXord <xordii.xd@gmail.com>
2021-09-18 08:45:27 +0200

    CW_3 Commit

--- Studia_Project/src/Main.java
+++ Studia_Project/src/Main.java
@@ -3,13 +3,40 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+        /* ćw.3 */
+
+        /*  zad.1 napisać interfejs który bedzie słuzył do wpisania danych osobowych:
+         *   a) Imię, nazwisko, wiek, nr indeksu,
+         *   dane mają być wprowadzane z klawiatury w konsoli
+         *   b) wyświetlić dane za pomocą println i printf
+         */
+        //a
         Scanner sc = new Scanner(System.in);
-        System.out.println("Podaj tekst: ");
+        System.out.println("Podaj swoje imie: ");
         String name = sc.nextLine();
-        System.out.println("Podaj liczbę: ");
-        int number = sc.nextInt();
-        System.out.println("Tekst: " + name + ", " + "Liczba: " + number);
-        System.out.printf("Tekst: %s, Liczba: %d",name,number);
+        System.out.println("Podaj swoje nazwisko: ");
+        String surName = sc.nextLine();
+        System.out.println("Podaj swój wiek: ");
+        int age = sc.nextInt();
+        System.out.println("Podaj swój numer indeksu: ");
+        int indexNumber = sc.nextInt();
+        //b
+        System.out.println("Imie: "+name+", Nazwisko: "+surName+", Wiek: "+age+", Numer indeksu: " +indexNumber);
+        System.out.printf("Imie: %s, Nazwisko: %s, Wiek: %d, Numer indeksu: %d",name,surName,age,indexNumber);
+
+
+
+        /*  zad.2 zadeklarować 2 zmienne typu int i wykonać na nich obliczenia arytmetyczn (+, -, *, /, %)
+         *   wynik wyświetlić w konsoli programu
+         */
+        int x = 10,y = 5;
+        System.out.println("Dodawanie: " +(x + y));
+        System.out.println("Odejmowanie: "+(x - y));
+        System.out.println("Mnożenie: "+(x * y));
+        System.out.println("Dzielenie: " +(x / y));
+        System.out.println("Reszta z dzielenia: "+(x % y));
+
+
 
     }
 
