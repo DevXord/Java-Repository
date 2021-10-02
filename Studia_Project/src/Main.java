34887e9a310b9834964b295fe8165b36dbc6298b
DevXord <xordii.xd@gmail.com>
2021-10-02 18:51:48 +0200

    CW_1 Commit

--- Studia_Project/src/Main.java
+++ Studia_Project/src/Main.java
@@ -3,13 +3,23 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Podaj tekst: ");
-        String name = sc.nextLine();
-        System.out.println("Podaj liczbę: ");
-        int number = sc.nextInt();
-        System.out.println("Tekst: " + name + ", " + "Liczba: " + number);
-        System.out.printf("Tekst: %s, Liczba: %d",name,number);
+        //        ćw.1 napisać po kilka przykładów wypisywania tekstu w konsoli,
+        //        Użyć każdej z opcji
+
+        System.out.println("text 1");
+        System.out.println("To jest przykladowy tekst");
+        System.out.println("ALA MA KOT ");
+        System.out.println("KOT MA ALE");
+        System.out.println("HELLO WORLD\n------------");
+
+
+
+        System.out.printf("HELLO %s\n","world");
+        System.out.printf("ALA MA KOTA  %s\n"," KOT MA ALE");
+        System.out.printf("%s %s\n","Hello","world");
+        System.out.printf("I have %d days\n",2);
+        System.out.printf("Thi is %s\n",true);
+
 
     }
 
